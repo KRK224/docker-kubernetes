db61d01c44541764d6635ea55a063e817c12146a
김경륜 <kyeongryunkim@i-bricks.co.kr>
2022-08-27 19:39:02 +0900

    ch54. nodemon 테스트

--- Session3/data-volumes-01-starting-setup/server.js
+++ Session3/data-volumes-01-starting-setup/server.js
@@ -30,7 +30,7 @@ app.post('/create', async (req, res) => {
 
   const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
   const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
-
+  console.log('Creating the file!!!');
   await fs.writeFile(tempFilePath, content);
   exists(finalFilePath, async (exists) => {
     if (exists) {
